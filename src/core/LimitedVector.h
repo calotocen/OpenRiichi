c3c37ca1776941e8b767c13342bfc3172d797101
かろり <calotocen@gmail.com>
2015-09-06 18:08:15 +0900

    有限可変長配列を実装した。

---
+++ src/core/LimitedVector.h
@@ -0,0 +1,389 @@
+/*
+ * Copyright 2015 calotocen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+
+#include <cassert>
+#include <cstdint>
+#include <array>
+
+
+namespace openriichi
+{
+	/**
+	 * 固定長配列である。
+	 */
+	template<class T, size_t size, typename SizeType = uint8_t>
+	class LimitedVector
+	{
+	private:
+		/// 固定長配列型。
+		using Elements = typename std::array<T, size>;
+
+
+	public:
+		/// イテレータ型。
+		using Iterator = typename Elements::iterator;
+
+		/// Const イテレータ型。
+		using ConstIterator = typename Elements::const_iterator;
+
+
+	private:
+		/// 要素配列。
+		Elements m_elements;
+
+		/// 要素の個数。
+		SizeType m_elementsSize;
+
+
+	public:
+		LimitedVector();
+
+
+	public:
+		/**
+		 * 要素配列の先頭を指すイテレータを返す。
+		 *
+		 * @return 要素配列の先頭を指すイテレータ。
+		 */
+		Iterator getHeadIterator();
+
+		/**
+		 * 要素配列の先頭を指すイテレータを返す。
+		 *
+		 * @return 要素配列の先頭を指すイテレータ。
+		 */
+		ConstIterator getHeadConstIterator() const;
+
+		/**
+		 * 要素配列の末尾を指すイテレータを返す。
+		 *
+		 * @return 要素配列の末尾を指すイテレータ。
+		 */
+		Iterator getTailIterator();
+
+		/**
+		 * 要素配列の末尾を指すイテレータを返す。
+		 *
+		 * @return 要素配列の末尾を指すイテレータ。
+		 */
+		ConstIterator getTailConstIterator() const;
+
+		/**
+		 * 指定された位置にある要素を返す。
+		 *
+		 * @param[in] index 位置。
+		 * @return 要素。
+		 * @note 要素配列の範囲外を位置に指定した場合の動作は，未定義である。
+		 */
+		const T &get(size_t index) const;
+
+		/**
+		 * 先頭にある要素を返す。
+		 *
+		 * @return 要素。
+		 * @note 要素配列が空であった場合の動作は，未定義である。
+		 */
+		const T &getFirst() const;
+
+		/**
+		 * 末尾にある要素を返す。
+		 *
+		 * @return 要素。
+		 * @note 要素配列が空であった場合の動作は，未定義である。
+		 */
+		const T &getLast() const;
+
+		/**
+		 * 配列が空であるかを返す。
+		 *
+		 * @return 配列が空である場合は true を，それ以外の場合は false を返す。
+		 */
+		bool isEmpty() const;
+
+		/**
+		 * 要素の個数を返す。
+		 *
+		 * @return 要素の個数。
+		 */
+		size_t count() const;
+
+		/**
+		 * 要素を指定された位置に設定する。
+		 *
+		 * @param[in] index 位置。
+		 * @param[in] element 要素。
+		 * @note 要素配列の範囲外を位置に指定した場合の動作は，未定義である。
+		 */
+		void set(size_t index, const T &element);
+
+		/**
+		 * 末尾に要素を追加する。
+		 *
+		 * @param[in] element 要素。
+		 * @note 手要素の要素個数が既に最大個数であった場合の動作は，未定義である。
+		 */
+		void add(const T &element);
+
+		/**
+		 * 要素を指定された位置に挿入する。
+		 *
+		 * @param[in] iterator 位置。
+		 * @param[in] element 要素。
+		 * @return 挿入した要素を指すイテレータ。
+		 * @note 要素配列の範囲外を位置に指定した場合の動作は，未定義である。
+		 * @note 手要素の要素個数が既に最大個数であった場合の動作は，未定義である。
+		 */
+		Iterator insert(Iterator iterator, const T &element);
+
+		/**
+		 * 要素を指定された位置に挿入する。
+		 *
+		 * @param[in] index 位置。
+		 * @param[in] element 要素。
+		 * @note 要素配列の範囲外を位置に指定した場合の動作は，未定義である。
+		 * @note 手要素の要素個数が既に最大個数であった場合の動作は，未定義である。
+		 */
+		void insert(size_t index, const T &element);
+
+		/**
+		 * 指定された位置にある要素を削除する。
+		 *
+		 * @param[in] iterator 位置。
+		 * @return 削除した要素の次の要素を指すイテレータ。
+		 * @note 要素配列の範囲外を位置に指定した場合の動作は，未定義である。
+		 * @note 手要素の要素個数が既に最大個数であった場合の動作は，未定義である。
+		 */
+		Iterator remove(Iterator iterator);
+
+		/**
+		 * 指定された位置にある要素を削除する。
+		 *
+		 * @param[in] index 位置。
+		 * @note 要素配列の範囲外を位置に指定した場合の動作は，未定義である。
+		 * @note 手要素の要素個数が既に最大個数であった場合の動作は，未定義である。
+		 */
+		void remove(size_t index);
+
+		/**
+		 * 要素を全削除する。
+		 */
+		void clear();
+	};
+
+
+	template<class T, size_t size, typename SizeType>
+	LimitedVector<T, size, SizeType>::LimitedVector()
+	{
+		m_elementsSize = 0;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	typename LimitedVector<T, size, SizeType>::Iterator LimitedVector<T, size, SizeType>::getHeadIterator()
+	{
+		Iterator returnValue = m_elements.begin();		// 戻り値。
+
+		return returnValue;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	typename LimitedVector<T, size, SizeType>::ConstIterator LimitedVector<T, size, SizeType>::getHeadConstIterator() const
+	{
+		ConstIterator returnValue = m_elements.cbegin();		// 戻り値。
+
+		return returnValue;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	typename LimitedVector<T, size, SizeType>::Iterator LimitedVector<T, size, SizeType>::getTailIterator()
+	{
+		Iterator returnValue = m_elements.begin() + m_elementsSize;		// 戻り値。
+
+		return returnValue;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	typename LimitedVector<T, size, SizeType>::ConstIterator LimitedVector<T, size, SizeType>::getTailConstIterator() const
+	{
+		ConstIterator returnValue = m_elements.cbegin() + m_elementsSize;		// 戻り値。
+
+		return returnValue;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	const T &LimitedVector<T, size, SizeType>::get(size_t index) const
+	{
+		// デバッグ版の場合のみ，引数をチェックする。
+		assert(0 <= index && index < m_elementsSize);
+
+		// 要素を返す。
+		return m_elements[index];
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	const T &LimitedVector<T, size, SizeType>::getFirst() const
+	{
+		// デバッグ版の場合のみ，状態をチェックする。
+		assert(m_elementsSize > 0);
+
+		// 要素を返す。
+		return m_elements[0];
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	const T &LimitedVector<T, size, SizeType>::getLast() const
+	{
+		// デバッグ版の場合のみ，状態をチェックする。
+		assert(m_elementsSize > 0);
+
+		// 要素を返す。
+		return m_elements[m_elementsSize - 1];
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	inline bool LimitedVector<T, size, SizeType>::isEmpty() const
+	{
+		bool returnValue = m_elementsSize == 0;		// 戻り値。
+
+		return returnValue;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	size_t LimitedVector<T, size, SizeType>::count() const
+	{
+		size_t returnValue = m_elementsSize;		// 戻り値。
+
+		return returnValue;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	void LimitedVector<T, size, SizeType>::set(size_t index, const T &element)
+	{
+		// デバッグ版の場合のみ，引数をチェックする。
+		assert(0 <= index && index < m_elementsSize);
+
+		// 要素を設定する。
+		m_elements[index] = element;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	void LimitedVector<T, size, SizeType>::add(const T &element)
+	{
+		// デバッグ版の場合のみ，状態をチェックする。
+		assert(m_elementsSize < m_elements.max_size());
+
+		// 要素を追加する。
+		m_elements[m_elementsSize] = element;
+		++m_elementsSize;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	typename LimitedVector<T, size, SizeType>::Iterator LimitedVector<T, size, SizeType>::insert(Iterator iterator, const T &element)
+	{
+		// デバッグ版の場合のみ，引数をチェックする。
+		assert(m_elements.begin() <= iterator && iterator <= m_elements.begin() + m_elementsSize && m_elementsSize < m_elements.max_size());
+
+		// 挿入位置以降にある要素を一つ後ろにずらす。
+		if (iterator < m_elements.begin() + m_elementsSize) {
+			auto headIt = m_elements.begin();		// 要素配列の先頭を指すイテレータ。
+
+			for (auto it = headIt + m_elementsSize; it >= iterator && it > headIt; --it) {
+				*it = *(it - 1);
+			}
+		}
+
+		// 挿入位置に要素を設定する。
+		*iterator = element;
+		++m_elementsSize;
+
+		return iterator;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	void LimitedVector<T, size, SizeType>::insert(size_t index, const T &element)
+	{
+		// デバッグ版の場合のみ，引数をチェックする。
+		assert(0 <= index && index <= m_elementsSize && m_elementsSize < m_elements.max_size());
+
+		// 挿入位置以降にある要素を一つ後ろにずらす。
+		if (index < m_elementsSize) {
+			for (auto i = m_elementsSize; i >= index && i > 0; --i) {
+				m_elements[i] = m_elements[i - 1];
+			}
+		}
+
+		// 挿入位置に要素を設定する。
+		m_elements[index] = element;
+		++m_elementsSize;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	typename LimitedVector<T, size, SizeType>::Iterator LimitedVector<T, size, SizeType>::remove(Iterator iterator)
+	{
+		// デバッグ版の場合のみ，引数をチェックする。
+		assert(m_elements.begin() <= iterator && iterator < m_elements.begin() + m_elementsSize);
+
+		// 削除位置以降にある要素を一つ前にずらす。
+		for (auto it = iterator; it < m_elements.begin() + m_elementsSize - 1; ++it)
+		{
+			*it = *(it + 1);
+		}
+
+		// 要素の個数を一つ減らす。
+		--m_elementsSize;
+
+		return iterator;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	void LimitedVector<T, size, SizeType>::remove(size_t index)
+	{
+		// デバッグ版の場合のみ，引数をチェックする。
+		assert(0 <= index && index < m_elementsSize);
+
+		// 削除位置以降にある要素を一つ前にずらす。
+		for (auto i = index; i + 1 < m_elementsSize; ++i)
+		{
+			m_elements[i] = m_elements[i + 1];
+		}
+
+		// 要素の個数を一つ減らす。
+		--m_elementsSize;
+	}
+
+
+	template<class T, size_t size, typename SizeType>
+	void LimitedVector<T, size, SizeType>::clear()
+	{
+		m_elementsSize = 0;
+	}
+}
---
+++ test/core/LimitedVectorTest.cpp
@@ -0,0 +1,407 @@
+/*
+ * Copyright 2015 calotocen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>	
+#include <LimitedVector.h>
+
+
+namespace openriichi
+{
+	class LimitedVectorTest : public ::testing::Test {};
+
+
+	// 要素数の取得，要素の取得，要素の設定，要素の追加，およびイテレータをテストする。
+	TEST_F(LimitedVectorTest, testCountAndGetAndSetAndAddAndIteratorAndConstIterator)
+	{
+		LimitedVector<int, 10> limitedVector;
+		LimitedVector<int, 10>::Iterator iterator;
+		LimitedVector<int, 10>::ConstIterator constIterator;
+
+		// 生成直後の配列は空である。
+		ASSERT_EQ(0, limitedVector.count());
+		ASSERT_EQ(limitedVector.getHeadIterator(), limitedVector.getTailIterator());
+		ASSERT_EQ(limitedVector.getHeadConstIterator(), limitedVector.getTailConstIterator());
+
+		// 要素を追加する。
+		limitedVector.add(1);
+		ASSERT_EQ(1, limitedVector.count());
+		ASSERT_EQ(1, limitedVector.get(0));
+		ASSERT_EQ(1, limitedVector.getFirst());
+		ASSERT_EQ(1, limitedVector.getLast());
+		iterator = limitedVector.getHeadIterator();
+		ASSERT_EQ(1, *iterator++);
+		ASSERT_EQ(limitedVector.getTailIterator(), iterator);
+		constIterator = limitedVector.getHeadConstIterator();
+		ASSERT_EQ(1, *constIterator++);
+		ASSERT_EQ(limitedVector.getTailConstIterator(), constIterator);
+
+		// 要素を追加する。
+		limitedVector.add(2);
+		ASSERT_EQ(2, limitedVector.count());
+		ASSERT_EQ(1, limitedVector.get(0));
+		ASSERT_EQ(2, limitedVector.get(1));
+		ASSERT_EQ(1, limitedVector.getFirst());
+		ASSERT_EQ(2, limitedVector.getLast());
+		iterator = limitedVector.getHeadIterator();
+		ASSERT_EQ(1, *iterator++);
+		ASSERT_EQ(2, *iterator++);
+		ASSERT_EQ(limitedVector.getTailIterator(), iterator);
+		constIterator = limitedVector.getHeadConstIterator();
+		ASSERT_EQ(1, *constIterator++);
+		ASSERT_EQ(2, *constIterator++);
+		ASSERT_EQ(limitedVector.getTailConstIterator(), constIterator);
+
+		// 要素を追加する。
+		limitedVector.add(3);
+		limitedVector.add(4);
+		limitedVector.add(5);
+		limitedVector.add(6);
+		limitedVector.add(7);
+		limitedVector.add(8);
+		limitedVector.add(9);
+		limitedVector.add(10);
+		ASSERT_EQ(10, limitedVector.count());
+		ASSERT_EQ(1, limitedVector.get(0));
+		ASSERT_EQ(2, limitedVector.get(1));
+		ASSERT_EQ(3, limitedVector.get(2));
+		ASSERT_EQ(4, limitedVector.get(3));
+		ASSERT_EQ(5, limitedVector.get(4));
+		ASSERT_EQ(6, limitedVector.get(5));
+		ASSERT_EQ(7, limitedVector.get(6));
+		ASSERT_EQ(8, limitedVector.get(7));
+		ASSERT_EQ(9, limitedVector.get(8));
+		ASSERT_EQ(10, limitedVector.get(9));
+		ASSERT_EQ(1, limitedVector.getFirst());
+		ASSERT_EQ(10, limitedVector.getLast());
+		iterator = limitedVector.getHeadIterator();
+		ASSERT_EQ(1, *iterator++);
+		ASSERT_EQ(2, *iterator++);
+		ASSERT_EQ(3, *iterator++);
+		ASSERT_EQ(4, *iterator++);
+		ASSERT_EQ(5, *iterator++);
+		ASSERT_EQ(6, *iterator++);
+		ASSERT_EQ(7, *iterator++);
+		ASSERT_EQ(8, *iterator++);
+		ASSERT_EQ(9, *iterator++);
+		ASSERT_EQ(10, *iterator++);
+		ASSERT_EQ(limitedVector.getTailIterator(), iterator);
+		constIterator = limitedVector.getHeadConstIterator();
+		ASSERT_EQ(1, *constIterator++);
+		ASSERT_EQ(2, *constIterator++);
+		ASSERT_EQ(3, *constIterator++);
+		ASSERT_EQ(4, *constIterator++);
+		ASSERT_EQ(5, *constIterator++);
+		ASSERT_EQ(6, *constIterator++);
+		ASSERT_EQ(7, *constIterator++);
+		ASSERT_EQ(8, *constIterator++);
+		ASSERT_EQ(9, *constIterator++);
+		ASSERT_EQ(10, *constIterator++);
+		ASSERT_EQ(limitedVector.getTailConstIterator(), constIterator);
+
+		// 要素を設定する。
+		limitedVector.set(0, 11);
+		limitedVector.set(5, 12);
+		limitedVector.set(9, 13);
+		ASSERT_EQ(10, limitedVector.count());
+		ASSERT_EQ(11, limitedVector.get(0));
+		ASSERT_EQ(2, limitedVector.get(1));
+		ASSERT_EQ(3, limitedVector.get(2));
+		ASSERT_EQ(4, limitedVector.get(3));
+		ASSERT_EQ(5, limitedVector.get(4));
+		ASSERT_EQ(12, limitedVector.get(5));
+		ASSERT_EQ(7, limitedVector.get(6));
+		ASSERT_EQ(8, limitedVector.get(7));
+		ASSERT_EQ(9, limitedVector.get(8));
+		ASSERT_EQ(13, limitedVector.get(9));
+		iterator = limitedVector.getHeadIterator();
+		ASSERT_EQ(11, *iterator++);
+		ASSERT_EQ(2, *iterator++);
+		ASSERT_EQ(3, *iterator++);
+		ASSERT_EQ(4, *iterator++);
+		ASSERT_EQ(5, *iterator++);
+		ASSERT_EQ(12, *iterator++);
+		ASSERT_EQ(7, *iterator++);
+		ASSERT_EQ(8, *iterator++);
+		ASSERT_EQ(9, *iterator++);
+		ASSERT_EQ(13, *iterator++);
+		ASSERT_EQ(limitedVector.getTailIterator(), iterator);
+		constIterator = limitedVector.getHeadConstIterator();
+		ASSERT_EQ(11, *constIterator++);
+		ASSERT_EQ(2, *constIterator++);
+		ASSERT_EQ(3, *constIterator++);
+		ASSERT_EQ(4, *constIterator++);
+		ASSERT_EQ(5, *constIterator++);
+		ASSERT_EQ(12, *constIterator++);
+		ASSERT_EQ(7, *constIterator++);
+		ASSERT_EQ(8, *constIterator++);
+		ASSERT_EQ(9, *constIterator++);
+		ASSERT_EQ(13, *constIterator++);
+		ASSERT_EQ(limitedVector.getTailConstIterator(), constIterator);
+	}
+
+	// 要素の挿入 (イテレータ版) をテストする。
+	TEST_F(LimitedVectorTest, testInsertUsingIterator)
+	{
+		LimitedVector<int, 10> limitedVector;
+		LimitedVector<int, 10>::Iterator iterator;
+
+		// 要素を挿入する。
+		iterator = limitedVector.insert(limitedVector.getHeadIterator(), 1);
+		ASSERT_EQ(limitedVector.getHeadIterator(), iterator);
+		ASSERT_EQ(1, limitedVector.count());
+		ASSERT_EQ(1, limitedVector.get(0));
+
+		// 要素を先頭に挿入する。
+		iterator = limitedVector.insert(limitedVector.getHeadIterator(), 2);
+		ASSERT_EQ(limitedVector.getHeadIterator(), iterator);
+		ASSERT_EQ(2, limitedVector.count());
+		ASSERT_EQ(2, limitedVector.get(0));
+		ASSERT_EQ(1, limitedVector.get(1));
+
+		// 要素を末尾に挿入する。
+		iterator = limitedVector.insert(limitedVector.getTailIterator(), 3);
+		ASSERT_EQ(limitedVector.getTailIterator() - 1, iterator);
+		ASSERT_EQ(3, limitedVector.count());
+		ASSERT_EQ(2, limitedVector.get(0));
+		ASSERT_EQ(1, limitedVector.get(1));
+		ASSERT_EQ(3, limitedVector.get(2));
+
+		// 要素を間に挿入する。
+		iterator = limitedVector.insert(limitedVector.getHeadIterator() + 1, 4);
+		iterator = limitedVector.insert(iterator + 1, 5);
+		iterator = limitedVector.insert(iterator + 1, 6);
+		iterator = limitedVector.insert(iterator + 1, 7);
+		iterator = limitedVector.insert(iterator + 1, 8);
+		iterator = limitedVector.insert(iterator + 1, 9);
+		iterator = limitedVector.insert(iterator + 1, 10);
+		ASSERT_EQ(limitedVector.getHeadIterator() + 7, iterator);
+		ASSERT_EQ(10, limitedVector.count());
+		ASSERT_EQ(2, limitedVector.get(0));
+		ASSERT_EQ(4, limitedVector.get(1));
+		ASSERT_EQ(5, limitedVector.get(2));
+		ASSERT_EQ(6, limitedVector.get(3));
+		ASSERT_EQ(7, limitedVector.get(4));
+		ASSERT_EQ(8, limitedVector.get(5));
+		ASSERT_EQ(9, limitedVector.get(6));
+		ASSERT_EQ(10, limitedVector.get(7));
+		ASSERT_EQ(1, limitedVector.get(8));
+		ASSERT_EQ(3, limitedVector.get(9));
+	}
+
+	// 要素の挿入 (添え字版) をテストする。
+	TEST_F(LimitedVectorTest, testInsertUsingIndex)
+	{
+		LimitedVector<int, 10> limitedVector;
+
+		// 要素を挿入する。
+		limitedVector.insert(0, 1);
+		ASSERT_EQ(1, limitedVector.count());
+		ASSERT_EQ(1, limitedVector.get(0));
+
+		// 要素を先頭に挿入する。
+		limitedVector.insert(0, 2);
+		ASSERT_EQ(2, limitedVector.count());
+		ASSERT_EQ(2, limitedVector.get(0));
+		ASSERT_EQ(1, limitedVector.get(1));
+
+		// 要素を末尾に挿入する。
+		limitedVector.insert(limitedVector.count(), 3);
+		ASSERT_EQ(3, limitedVector.count());
+		ASSERT_EQ(2, limitedVector.get(0));
+		ASSERT_EQ(1, limitedVector.get(1));
+		ASSERT_EQ(3, limitedVector.get(2));
+
+		// 要素を間に挿入する。
+		limitedVector.insert(1, 4);
+		limitedVector.insert(2, 5);
+		limitedVector.insert(3, 6);
+		limitedVector.insert(4, 7);
+		limitedVector.insert(5, 8);
+		limitedVector.insert(6, 9);
+		limitedVector.insert(7, 10);
+		ASSERT_EQ(10, limitedVector.count());
+		ASSERT_EQ(2, limitedVector.get(0));
+		ASSERT_EQ(4, limitedVector.get(1));
+		ASSERT_EQ(5, limitedVector.get(2));
+		ASSERT_EQ(6, limitedVector.get(3));
+		ASSERT_EQ(7, limitedVector.get(4));
+		ASSERT_EQ(8, limitedVector.get(5));
+		ASSERT_EQ(9, limitedVector.get(6));
+		ASSERT_EQ(10, limitedVector.get(7));
+		ASSERT_EQ(1, limitedVector.get(8));
+		ASSERT_EQ(3, limitedVector.get(9));
+	}
+
+	// 要素の削除 (イテレータ版) と要素の全削除をテストする。
+	TEST_F(LimitedVectorTest, testRemoveUsingIndexAndClear)
+	{
+		LimitedVector<int, 10> limitedVector;
+		LimitedVector<int, 10>::Iterator iterator;
+
+		// 要素を削除する。
+		limitedVector.add(1);
+		iterator = limitedVector.remove(limitedVector.getHeadIterator());
+		ASSERT_EQ(limitedVector.getHeadIterator(), iterator);
+		ASSERT_EQ(0, limitedVector.count());
+
+		// 先頭の要素を削除する。
+		limitedVector.add(2);
+		limitedVector.add(3);
+		iterator = limitedVector.remove(limitedVector.getHeadIterator());
+		ASSERT_EQ(limitedVector.getHeadIterator(), iterator);
+		ASSERT_EQ(1, limitedVector.count());
+		ASSERT_EQ(3, limitedVector.get(0));
+
+		// 末尾の要素を削除する。
+		limitedVector.insert(limitedVector.getHeadIterator(), 4);
+		iterator = limitedVector.remove(limitedVector.getTailIterator() - 1);
+		ASSERT_EQ(limitedVector.getTailIterator(), iterator);
+		ASSERT_EQ(1, limitedVector.count());
+		ASSERT_EQ(4, limitedVector.get(0));
+
+		// 間の要素を削除する。
+		limitedVector.add(5);
+		limitedVector.add(6);
+		limitedVector.add(7);
+		limitedVector.add(8);
+		limitedVector.add(9);
+		limitedVector.add(10);
+		limitedVector.add(11);
+		limitedVector.add(12);
+		limitedVector.add(13);
+		limitedVector.remove(limitedVector.getHeadIterator() + 1);
+		limitedVector.remove(limitedVector.getHeadIterator() + 3);
+		limitedVector.remove(limitedVector.getHeadIterator() + 5);
+		ASSERT_EQ(7, limitedVector.count());
+		ASSERT_EQ(4, limitedVector.get(0));
+		ASSERT_EQ(6, limitedVector.get(1));
+		ASSERT_EQ(7, limitedVector.get(2));
+		ASSERT_EQ(9, limitedVector.get(3));
+		ASSERT_EQ(10, limitedVector.get(4));
+		ASSERT_EQ(12, limitedVector.get(5));
+		ASSERT_EQ(13, limitedVector.get(6));
+
+		// 先頭の要素を削除する (要素配列サイズ最大時)
+		limitedVector.add(14);
+		limitedVector.add(15);
+		limitedVector.add(16);
+		limitedVector.remove(limitedVector.getHeadIterator());
+		ASSERT_EQ(9, limitedVector.count());
+		ASSERT_EQ(6, limitedVector.get(0));
+		ASSERT_EQ(7, limitedVector.get(1));
+		ASSERT_EQ(9, limitedVector.get(2));
+		ASSERT_EQ(10, limitedVector.get(3));
+		ASSERT_EQ(12, limitedVector.get(4));
+		ASSERT_EQ(13, limitedVector.get(5));
+		ASSERT_EQ(14, limitedVector.get(6));
+		ASSERT_EQ(15, limitedVector.get(7));
+		ASSERT_EQ(16, limitedVector.get(8));
+
+		// 末尾の要素を削除する (要素配列サイズ最大時)
+		limitedVector.insert(limitedVector.getHeadIterator(), 17);
+		limitedVector.remove(limitedVector.getTailIterator() - 1);
+		ASSERT_EQ(9, limitedVector.count());
+		ASSERT_EQ(17, limitedVector.get(0));
+		ASSERT_EQ(6, limitedVector.get(1));
+		ASSERT_EQ(7, limitedVector.get(2));
+		ASSERT_EQ(9, limitedVector.get(3));
+		ASSERT_EQ(10, limitedVector.get(4));
+		ASSERT_EQ(12, limitedVector.get(5));
+		ASSERT_EQ(13, limitedVector.get(6));
+		ASSERT_EQ(14, limitedVector.get(7));
+		ASSERT_EQ(15, limitedVector.get(8));
+
+		// 要素を全削除する。
+		limitedVector.clear();
+		ASSERT_EQ(0, limitedVector.count());
+	}
+
+	// 要素の削除 (添え字版) をテストする。
+	TEST_F(LimitedVectorTest, testRemoveUsingIndex)
+	{
+		LimitedVector<int, 10> limitedVector;
+
+		// 要素を削除する。
+		limitedVector.add(1);
+		limitedVector.remove(0);
+		ASSERT_EQ(0, limitedVector.count());
+
+		// 先頭の要素を削除する。
+		limitedVector.add(2);
+		limitedVector.add(3);
+		limitedVector.remove(0);
+		ASSERT_EQ(1, limitedVector.count());
+		ASSERT_EQ(3, limitedVector.get(0));
+
+		// 末尾の要素を削除する。
+		limitedVector.insert(0, 4);
+		limitedVector.remove(limitedVector.count() - 1);
+		ASSERT_EQ(1, limitedVector.count());
+		ASSERT_EQ(4, limitedVector.get(0));
+
+		// 間の要素を削除する。
+		limitedVector.add(5);
+		limitedVector.add(6);
+		limitedVector.add(7);
+		limitedVector.add(8);
+		limitedVector.add(9);
+		limitedVector.add(10);
+		limitedVector.add(11);
+		limitedVector.add(12);
+		limitedVector.add(13);
+		limitedVector.remove(1);
+		limitedVector.remove(3);
+		limitedVector.remove(5);
+		ASSERT_EQ(7, limitedVector.count());
+		ASSERT_EQ(7, limitedVector.count());
+		ASSERT_EQ(4, limitedVector.get(0));
+		ASSERT_EQ(6, limitedVector.get(1));
+		ASSERT_EQ(7, limitedVector.get(2));
+		ASSERT_EQ(9, limitedVector.get(3));
+		ASSERT_EQ(10, limitedVector.get(4));
+		ASSERT_EQ(12, limitedVector.get(5));
+		ASSERT_EQ(13, limitedVector.get(6));
+
+		// 先頭の要素を削除する (要素配列サイズ最大時)
+		limitedVector.add(14);
+		limitedVector.add(15);
+		limitedVector.add(16);
+		limitedVector.remove(0);
+		ASSERT_EQ(9, limitedVector.count());
+		ASSERT_EQ(6, limitedVector.get(0));
+		ASSERT_EQ(7, limitedVector.get(1));
+		ASSERT_EQ(9, limitedVector.get(2));
+		ASSERT_EQ(10, limitedVector.get(3));
+		ASSERT_EQ(12, limitedVector.get(4));
+		ASSERT_EQ(13, limitedVector.get(5));
+		ASSERT_EQ(14, limitedVector.get(6));
+		ASSERT_EQ(15, limitedVector.get(7));
+		ASSERT_EQ(16, limitedVector.get(8));
+
+		// 末尾の要素を削除する (要素配列サイズ最大時)
+		limitedVector.insert(limitedVector.getHeadIterator(), 17);
+		limitedVector.remove(limitedVector.count() - 1);
+		ASSERT_EQ(9, limitedVector.count());
+		ASSERT_EQ(17, limitedVector.get(0));
+		ASSERT_EQ(6, limitedVector.get(1));
+		ASSERT_EQ(7, limitedVector.get(2));
+		ASSERT_EQ(9, limitedVector.get(3));
+		ASSERT_EQ(10, limitedVector.get(4));
+		ASSERT_EQ(12, limitedVector.get(5));
+		ASSERT_EQ(13, limitedVector.get(6));
+		ASSERT_EQ(14, limitedVector.get(7));
+		ASSERT_EQ(15, limitedVector.get(8));
+	}
+}
