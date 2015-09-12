049d775e1e0a21b049abf318180b4e1a02ab23a4
かろり <calotocen@gmail.com>
2015-09-12 15:56:52 +0900

    テストを追加／変更，および整理した。

--- test/core/HandTest.cpp
+++ test/core/HandTest.cpp
@@ -13,58 +13,147 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <vector>
 #include <gtest/gtest.h>	
-#include <SimpleHand.h>
+#include <Hand.h>
+#include <Tiles.h>
+
+
+using namespace std;
 
 
 namespace openriichi
 {
-	class SimpleHandTest : public ::testing::Test {};
+	class HandTest : public ::testing::Test {};
 
-	/// インスタンスのサイズをテストする。
-	TEST_F(SimpleHandTest, testSizeOfInstance)
+	/// コンストラクタ (牌配列，面子配列指定) をテストする。
+	TEST_F(HandTest, testConstructorWithTilesAndSets)
 	{
-		SimpleHand hand;
-		ASSERT_EQ(46, sizeof(hand));
+		Hand hand(Hand::Tiles{ HK, HK, HK }, Hand::Sets{ Set(HT, HT, HT) });
+
+		ASSERT_EQ((Hand::Tiles{ HK, HK, HK }), hand.tiles());
+		ASSERT_EQ((Hand::Sets{ Set(HT, HT, HT) }), hand.sets());
 	}
 
-	// 手牌をテストする。
-	TEST_F(SimpleHandTest, testHand)
+	/// tiles 関数をテストする。
+	TEST_F(HandTest, testTiles)
 	{
-		SimpleHand hand;
+		Hand hand;
+		Hand::Tiles tiles;
 
-		SimpleHand::Tiles &tiles = hand.tiles();
-		ASSERT_EQ(SimpleHand::Tiles(), tiles);
+		ASSERT_EQ(tiles, hand.tiles());
 
-		SimpleHand::Sets &sets = hand.sets();
-		ASSERT_EQ(SimpleHand::Sets(), sets);
+		hand.tiles() = { P1, P2, P3, S1, S2, S3, M1, M2, M3, TN, TN, TN, NN };
+		tiles = { P1, P2, P3, S1, S2, S3, M1, M2, M3, TN, TN, TN, NN };
+		ASSERT_EQ(tiles, hand.tiles());
+
+		hand.tiles().add(NN);
+		tiles.add(NN);
+		ASSERT_EQ(tiles, hand.tiles());
+
+		hand.tiles().remove(0);
+		tiles.remove(0);
+		ASSERT_EQ(tiles, hand.tiles());
 	}
 
-	// 手牌の比較をテストする。
-	TEST_F(SimpleHandTest, testEqualsAndNotEquals)
+	/// sets 関数をテストする。
+	TEST_F(HandTest, testSets)
 	{
-		SimpleHand hand1;
-		SimpleHand hand2;
+		Hand hand;
+		Hand::Sets sets;
 
+		ASSERT_EQ(sets, hand.sets());
+
+		hand.sets() = { Set(P1, P2, P3), Set(S1, S2, S3), Set(M1, M2, M3), Set(TN, TN, TN, TN) };
+		sets = { Set(P1, P2, P3), Set(S1, S2, S3), Set(M1, M2, M3), Set(TN, TN, TN, TN) };
+		ASSERT_EQ(sets, hand.sets());
+
+		hand.sets().add(Set(NN, NN));
+		sets.add(Set(NN, NN));
+		ASSERT_EQ(sets, hand.sets());
+
+		hand.sets().remove(0);
+		sets.remove(0);
+		ASSERT_EQ(sets, hand.sets());
+	}
+
+	/// == 演算子をテストする。
+	TEST_F(HandTest, testEquals)
+	{
+		Hand hand1;
+		Hand hand2{ CH, CH, CH };
+		Hand hand3{ Set(CH, CH, CH) };
+		Hand hand4(Hand::Tiles{ CH, CH, CH }, Hand::Sets{ Set(CH, CH, CH) });
+
+		// 自身と比較する。
 		ASSERT_TRUE(hand1 == hand1);
+		ASSERT_TRUE(hand2 == hand2);
+		ASSERT_TRUE(hand3 == hand3);
+		ASSERT_TRUE(hand4 == hand4);
+
+		// 同じ手牌と比較する。
+		ASSERT_TRUE(Hand() == hand1);
+		ASSERT_TRUE((Hand{ CH, CH, CH }) == hand2);
+		ASSERT_TRUE((Hand{ Set(CH, CH, CH) }) == hand3);
+		ASSERT_TRUE(Hand(Hand::Tiles{ CH, CH, CH }, Hand::Sets{ Set(CH, CH, CH) }) == hand4);
+
+		// 異なる手牌と比較する。
+		vector<Hand> hands{ hand1, hand2, hand3, hand4 };
+		for (auto i = 0; i < hands.size(); ++i) {
+			for (auto j = 0; j < hands.size(); ++j) {
+				if (i != j) {
+					ASSERT_FALSE(hands[i] == hands[j]);
+				}
+			}
+		}
+	}
+
+	/// != 演算子をテストする。
+	TEST_F(HandTest, testNotEquals)
+	{
+		Hand hand1;
+		Hand hand2{ CH, CH, CH };
+		Hand hand3{ Set(CH, CH, CH) };
+		Hand hand4(Hand::Tiles{ CH, CH, CH }, Hand::Sets{ Set(CH, CH, CH) });
+
+		// 自身と比較する。
 		ASSERT_FALSE(hand1 != hand1);
-		ASSERT_TRUE(hand1 == hand2);
-		ASSERT_FALSE(hand1 != hand2);
+		ASSERT_FALSE(hand2 != hand2);
+		ASSERT_FALSE(hand3 != hand3);
+		ASSERT_FALSE(hand4 != hand4);
 
-		hand1.tiles().add(Tile());
-		ASSERT_FALSE(hand1 == hand2);
-		ASSERT_TRUE(hand1 != hand2);
+		// 同じ手牌と比較する。
+		ASSERT_FALSE(Hand() != hand1);
+		ASSERT_FALSE((Hand{ CH, CH, CH }) != hand2);
+		ASSERT_FALSE((Hand{ Set(CH, CH, CH) }) != hand3);
+		ASSERT_FALSE(Hand(Hand::Tiles{ CH, CH, CH }, Hand::Sets{ Set(CH, CH, CH) }) != hand4);
 
-		hand2.tiles().add(Tile());
-		ASSERT_TRUE(hand1 == hand2);
-		ASSERT_FALSE(hand1 != hand2);
+		// 異なる手牌と比較する。
+		vector<Hand> hands{ hand1, hand2, hand3, hand4 };
+		for (auto i = 0; i < hands.size(); ++i) {
+			for (auto j = 0; j < hands.size(); ++j) {
+				if (i != j) {
+					ASSERT_TRUE(hands[i] != hands[j]);
+				}
+			}
+		}
+	}
 
-		hand1.sets().add(Set());
-		ASSERT_FALSE(hand1 == hand2);
-		ASSERT_TRUE(hand1 != hand2);
+	/// = 演算子 (牌配列用) をテストする。
+	TEST_F(HandTest, testSubstituteTiles)
+	{
+		Hand hand;
 
-		hand2.sets().add(Set());
-		ASSERT_TRUE(hand1 == hand2);
-		ASSERT_FALSE(hand1 != hand2);
+		hand = { P1, P2, P3 };
+		ASSERT_EQ((Hand{ P1, P2, P3 }), hand);
+	}
+
+	/// = 演算子 (面子配列用) をテストする。
+	TEST_F(HandTest, testSubstituteSets)
+	{
+		Hand hand;
+
+		hand = { Set(P1, P2, P3) };
+		ASSERT_EQ((Hand{ Set(P1, P2, P3) }), hand);
 	}
 }
