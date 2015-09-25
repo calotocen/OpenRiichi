8ad4cd9b96c8e14d1422e588dad6f7a3028617eb
かろり <calotocen@gmail.com>
2015-09-26 07:14:20 +0900

    マウスイベントを表すクラスを実装した。

---
+++ src/server/mfcdialog/MouseEvent.cpp
@@ -0,0 +1,52 @@
+﻿/*
+ * Copyright 2015 calotocen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "stdafx.h"
+#include "MouseEvent.h"
+
+
+MouseEvent::MouseEvent(Type type, const CPoint &point)
+	: MouseEvent(type, NO_BUTTON, point)
+{
+	// 何もしない。
+}
+
+
+MouseEvent::MouseEvent(Type type, Button button, const CPoint &point)
+	: m_type(type)
+	, m_button(button)
+	, m_point(point)
+{
+	// アサーションが有効である場合のみ，引数をチェックする。
+	openriichi_assert(m_type == MOVED && m_button == NO_BUTTON);
+}
+
+
+MouseEvent::Type MouseEvent::getType() const
+{
+	return m_type;
+}
+
+
+MouseEvent::Button MouseEvent::getButton() const
+{
+	return m_button;
+}
+
+
+const CPoint &MouseEvent::getPoint() const
+{
+	return m_point;
+}
---
+++ src/server/mfcdialog/MouseEvent.h
@@ -0,0 +1,102 @@
+﻿/*
+ * Copyright 2015 calotocen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/**
+ * マウスイベントである。
+ */
+class MouseEvent
+{
+public:
+	/**
+	 * イベント種別である。
+	 */
+	enum Type
+	{
+		/// 移動。
+		MOVED,
+
+		/// クリック。
+		CLICKED,
+	};
+
+	/**
+	 * ボタンである。
+	 */
+	enum Button
+	{
+		/// なし。
+		NO_BUTTON,
+
+		/// 左ボタン。
+		LEFT_BUTTON,
+
+		/// 右ボタン。
+		RIGHT_BUTTON,
+	};
+
+
+private:
+	/// イベント種別。
+	Type m_type;
+
+	/// ボタン。
+	Button m_button;
+
+	/// イベント発生場所。
+	CPoint m_point;
+
+
+public:
+	/**
+	 * マウスイベントを生成する。
+	 *
+	 * @param[in] type イベント種別。
+	 * @param[in] point イベント発生場所。
+	 */
+	MouseEvent(Type type, const CPoint &point);
+
+	/**
+	 * マウスイベントを生成する。
+	 *
+	 * @param[in] type イベント種別。
+	 * @param[in] button ボタン。
+	 * @param[in] point イベント発生場所。
+	 */
+	MouseEvent(Type type, Button button, const CPoint &point);
+
+	/**
+	 * イベント種別を返す。
+	 *
+	 * @return イベント種別。
+	 */
+	Type getType() const;
+
+	/**
+	 * ボタンを返す。
+	 *
+	 * @return ボタン。
+	 */
+	Button getButton() const;
+
+	/**
+	 * イベント発生場所を返す。
+	 *
+	 * @return イベント発生場所。
+	 * @note 本関数の戻り値は，少なくとも本インスタンスが生存している間，有効である。
+	 */
+	const CPoint &getPoint() const;
+};
